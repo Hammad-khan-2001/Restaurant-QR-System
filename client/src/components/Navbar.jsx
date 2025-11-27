6662d11949c9cc8614eb593ce1fbfbeef04e5ccd
Hammad-khan-2001 <hammadkhan4513@gmail.com>
2025-11-28 00:28:31 +0530

    title icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0 text-2xl font-bold">
-            ScanDine
+            Qrovia
           </div>
 
           {/* Desktop Menu */}
