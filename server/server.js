acb72e675fa40e1e0c95ee5618b6a0b5d582fd44
Hammad-khan-2001 <hammadkhan4513@gmail.com>
2025-12-02 14:23:42 +0530

    server change

--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT ;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;
