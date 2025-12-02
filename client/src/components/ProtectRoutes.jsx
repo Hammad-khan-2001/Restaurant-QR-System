5fb6593272e6d58fbece89a16460902b3dd2e352
Hammad-khan-2001 <hammadkhan4513@gmail.com>
2025-12-02 21:55:28 +0530

    debug

--- client/src/components/ProtectRoutes.jsx
+++ client/src/components/ProtectRoutes.jsx
@@ -3,8 +3,9 @@ import { Navigate } from "react-router-dom";
 
 const ProtectRoutes = ({ children }) => {
 
-  const token =
-    useSelector((state) => state.auth.token) || localStorage.getItem("token");
+  const token = useSelector((state) => state.auth.token) || localStorage.getItem("token");
+
+  console.log("ProtectRoutes token 👉", token);
 
   if (!token) {
     return <Navigate to="/login" replace />;
