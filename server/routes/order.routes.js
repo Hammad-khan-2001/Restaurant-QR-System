4ad9a89ced8b3f87a7c7bd98dedb5e5f1e471cb4
Hammad-khan-2001 <hammadkhan4513@gmail.com>
2025-12-15 22:27:34 +0530

    order route change

--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -12,21 +12,38 @@ import {
   markOrderPaid,
   getUserOrders
 } from "../controllers/order.controller.js";
-import verifyToken  from "../middlewares/verifyToken.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
+// router.post("/place", verifyToken, placeOrder);
+// router.get("/", getAllOrders);
+// router.get("/table/:tableNumber", getOrdersByTable);
+// router.put("/status/:id", updateOrderStatus);
+// router.put("/cancel/:id", cancelOrder);
+// router.put("/complete/:id", completeOrder);
+// router.get("/live", getLiveOrders);
+// router.get("/:id", getOrderById);
+// router.put("/:id/payment-method",setPaymentMethod);
+// router.put("/:id/mark-paid",markOrderPaid);
+// router.get("/user", verifyToken, getUserOrders);
+
 router.post("/place", verifyToken, placeOrder);
+
+router.get("/user", verifyToken, getUserOrders); 
+
 router.get("/", getAllOrders);
 router.get("/table/:tableNumber", getOrdersByTable);
+router.get("/live", getLiveOrders);
+
 router.put("/status/:id", updateOrderStatus);
 router.put("/cancel/:id", cancelOrder);
 router.put("/complete/:id", completeOrder);
-router.get("/live", getLiveOrders);
-router.get("/:id", getOrderById);
-router.put("/:id/payment-method",setPaymentMethod);
-router.put("/:id/mark-paid",markOrderPaid);
-router.get("/user", verifyToken, getUserOrders);
+
+router.put("/:id/payment-method", setPaymentMethod);
+router.put("/:id/mark-paid", markOrderPaid);
+
+router.get("/:id", getOrderById); 
 
 
 export default router;
